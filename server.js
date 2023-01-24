8c1c8827611865b8558478b318df6fc533f2733d
mandoe310 <mandoe1@yahoo.com>
2023-01-23 21:52:36 -0800

    fixes double code

--- server.js
+++ server.js
@@ -31,15 +31,9 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(function (req, res, next) {
-  res.locals.user = req.user;
-  next();
-});
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize())
-app.use(passport.session())
 app.use(function (req, res, next) {
   res.locals.user = req.user;
   next();
